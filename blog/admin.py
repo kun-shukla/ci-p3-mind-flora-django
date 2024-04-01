7a462258803eebb15bc5d3b71405eee4923c1d64
Kunal S <kun.shukla@gmail.com>
2024-04-01 19:38:55 +0000

    Enhance admin panel using 'summernote'

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Lists fields for display in admin, fields for search,
+    field filters, fields to prepopulate and rich-text editor.
+    """
+    list_display = ('category', 'title', 'slug', 'status','created_on')
+    search_fields = ['title','content']
+    list_filter = ('status','category','created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
--- mind_flora/settings.py
+++ mind_flora/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','.herokuapp.com']
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
 ]
 
