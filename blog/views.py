54824a2a626c41e77e2fb1982e02d781a1347edf
Kunal S <kun.shukla@gmail.com>
2024-04-04 15:31:01 +0000

    Create base.html and index.html templates and apply styling

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,4 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
