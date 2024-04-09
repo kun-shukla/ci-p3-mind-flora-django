4126018082797303d4a8004f68d1a62b2e72dcdd
Kunal S <kun.shukla@gmail.com>
2024-04-09 10:08:42 +0000

    added post_detail view and update index.html template to link to the post detail page

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from . import views
 from django.urls import path
 
-urlpatterns = [ path('', views.post_list, name='home'),]
+urlpatterns = [ path('', views.post_list, name='home'),
+path('<slug:slug>/', views.post_detail, name='post_detail'), 
+]
